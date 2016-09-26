2d0d7a8a99c5706f66802947ab554121bb56eaa7
Syifa Dzikri Tsani <syifadzikri@gmail.com>
2016-09-26 21:30:43 +0700

    Add files via upload

---
+++ Praktikum_3-2.ino
@@ -0,0 +1,20 @@
+//Syifa Dzikri (22)
+//Wildan Firdaus (21)
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+
+void setup() { 
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0); 
+  lcd.print("ADC Example on A0"); 
+  analogReference(DEFAULT);
+  }
+void loop() {
+  unsigned int dataADC = analogRead(A0);
+  lcd.setCursor(0,1);
+  lcd.print("Value = ");
+  lcd.print(dataADC);
+  delay(500);
+  }
+
